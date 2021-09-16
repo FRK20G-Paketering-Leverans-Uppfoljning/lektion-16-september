aa2309c3128cc10b2c93be1c5ec7c297c437a02b
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2021-09-16 11:09:19 +0200

    Added code

---
+++ parcel-vue-example/src/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+      <h1>{{ message }}</h1>
+      <Name />
+  </section>
+</template>
+
+<script>
+import Name from './Name.vue';
+
+export default {
+    name: 'App',
+    components: {
+        Name
+    },
+    data() {
+        return {
+            message: 'Få en personlig hälsning!'
+        }
+    }
+}
+</script>
+
+<style>
+h1 {
+    color: purple;
+}
+</style>
---
+++ parcel-vue-example/src/components/Name.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+      <input type="text" placeholder="Ditt namn?" v-model="name">
+      <p>Hej {{ name }}!</p>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'Name',
+    data() {
+        return {
+            name: ''
+        }
+    }
+}
+</script>
+
+<style>
+ p {
+     color: #93f;
+ }
+</style>
